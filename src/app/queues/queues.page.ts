c8d1a71d4af0cd5f9c314475fe0169141fdcb1b2
Felipe <felipe.a.melo@hotmail.com>
2024-09-12 16:16:41 -0300

    Queues page - content and stylesheet - done

--- src/app/queues/queues.page.ts
+++ src/app/queues/queues.page.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./queues.page.scss'],
 })
 export class QueuesPage implements OnInit {
+  private queue: Array<number> = [1];
+  private newElement: number = 1;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  public getQueue(): Array<number> {
+    return this.queue;
+  }
+
+  public enqueue(): Array<number> {
+    this.newElement += 1;
+    this.queue.push(this.newElement);
+
+    return this.queue;
+  }
+
+  public dequeue(): Array<number> {
+    this.queue.shift()
+
+    return this.queue;
+  }
 }
